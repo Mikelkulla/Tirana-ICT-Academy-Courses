3a5508c16670ddaa975bda25818b726188974d57
Mikelkulla <94494461+Mikelkulla@users.noreply.github.com>
2024-08-18 22:12:02 +0200

    Add files via upload

---
+++ Free Code Camp/Legacy Python For Everyone/4. Tuples/1. Tuples.py
@@ -0,0 +1,4 @@
+lista = [1]
+tuple_ = (1, 2, 3, 4 )
+print(dir(lista))
+print(dir(tuple_))
---
+++ Free Code Camp/Legacy Python For Everyone/4. Tuples/4. Tuples are comparable.py
@@ -0,0 +1,5 @@
+print((1,2,3) > (5,1,2))
+print((0,1,2000000) > (0,3,4)) # The first check is the deicision if true or false
+print(("Jones", "Sally") > ("Jones", "Sam")) # Strings are compared letter by letter
+print(("Jones", "Sally") > ("Adam", "Sam"))
+
