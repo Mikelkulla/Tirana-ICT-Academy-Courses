d099dd95b02144312b308fe8c24ee7e70ce8cab3
Mikelkulla <94494461+Mikelkulla@users.noreply.github.com>
2024-10-10 23:31:04 +0200

    Add files via upload

---
+++ Free Code Camp/Legacy Python For Everyone/12. twfriends/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "aUM6O9qruZZAKyrYLJJdczbJa",
+            "consumer_secret": "wkGmE89mXz39z5O4ta9FeGoBsQk2gCdn1NarFQyXeIKjPMRTnn",
+            "token_key": "1596637509148188672-uxjDVy5cseichsqbpeWg51hk0Vvvey",
+            "token_secret": "TVabMC3ObGm3IPkVNtpcgSj7ZMTaAB66N6aTXKAGYbTlc"}
